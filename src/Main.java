2d6b12446b96c723af761911463652931be03af9
Md Rifad Ahmed <rifad.mbstu@gmail.com>
2023-03-05 00:15:36 +0100

    finding the vowel or consonant alphabet

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        char ch='c';
+        if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u' ){
+            System.out.println("The alphabet is vowel");
+        }else {
+            System.out.println("The alphabet is consonant");
+        }
+    }
+}
